c842a3b60128834e84309137a4c3aaacf8b48bc9
Swami Chandrasekaran <swamichandra@gmail.com>
2023-11-11 00:18:43 -0600

    Update Counter.tsx

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -4,11 +4,7 @@ import "./Counter.css";
 export default function Counter() {
   const [count, setCount] = createSignal(0);
   return (
-    <gen-search-widget
-      configId="23df4038-8851-4b5e-84ef-86eae2f5ecc4"
-      triggerId="searchWidgetTrigger">
-    </gen-search-widget>
-    
+   
     <input placeholder="Search here" id="searchWidgetTrigger" />
     
   );
