e4e65721eae709249b7f2a703d2ec4a69c367a65
Swami Chandrasekaran <swamichandra@gmail.com>
2023-11-11 00:16:34 -0600

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,13 @@
 import { Title } from "solid-start";
-import Counter from "~/components/Search";
+import Counter from "~/components/Counter";
 
 export default function Home() {
   return (
     <main>
       <Title>Student of the Client</Title>
-      <h3>Student of the Client</h3>
-      <Search />
+      <h3>Student of the Client!</h3>
+      <Counter />
+
+    </main>
+  );
 }
