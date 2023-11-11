7abd1bc81a886cb304d2cbfe0d42427cad6c15bf
Swami Chandrasekaran <swamichandra@gmail.com>
2023-11-11 00:17:18 -0600

    Update Counter.tsx

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -4,8 +4,8 @@ import "./Counter.css";
 export default function Counter() {
   const [count, setCount] = createSignal(0);
   return (
-    <button class="increment" onClick={() => setCount(count() + 1)}>
-      Clicks: {count()}
-    </button>
+  
+    <input placeholder="Search here" id="searchWidgetTrigger" />
+    
   );
 }
