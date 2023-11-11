f70336c2cbb74fab306e9b5dc859362794dbae54
Swami Chandrasekaran <swamichandra@gmail.com>
2023-11-11 00:07:27 -0600

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,10 @@
 import { Title } from "solid-start";
-import Counter from "~/components/Counter";
+import Counter from "~/components/Search";
 
 export default function Home() {
   return (
     <main>
       <Title>Student of the Client</Title>
       <h3>Student of the Client</h3>
-
-<gen-search-widget
-  configId="23df4038-8851-4b5e-84ef-86eae2f5ecc4"
-  triggerId="searchWidgetTrigger">
-</gen-search-widget>
-
-<input placeholder="Search here" id="searchWidgetTrigger" />
-    </main>
-  );
+      <Search />
 }
