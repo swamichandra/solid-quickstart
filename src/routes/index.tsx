9df01e446c7b3d883de3e6b9fdf38ac758684b25
Swami Chandrasekaran <swamichandra@gmail.com>
2023-11-11 00:22:38 -0600

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,14 @@ export default function Home() {
     <main>
       <Title>Student of the Client</Title>
       <h3>Student of the Client!</h3>
-      <Counter />
+     
+
+      <gen-search-widget
+            configId="23df4038-8851-4b5e-84ef-86eae2f5ecc4"
+            triggerId="searchWidgetTrigger">
+      </gen-search-widget>
+
+     <input placeholder="Search here" id="searchWidgetTrigger" />
 
     </main>
   );
