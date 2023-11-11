6ea880d2b44bfb37a6dacef51824bfa4537114fd
Swami Chandrasekaran <swamichandra@gmail.com>
2023-11-11 00:38:07 -0600

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,6 @@ export default function Home() {
     <main>
       <Title>Student of the Client</Title>
       <h3>Student of the Client!</h3>
-     
 
       <gen-search-widget
             configId="23df4038-8851-4b5e-84ef-86eae2f5ecc4"
@@ -17,24 +16,7 @@ export default function Home() {
             <button type="button">
                 <i class="fa fa-search" aria-hidden="true"></i>
             </button>
-     </form>
-
-
-        
-      <div class="search_box">
-         <div class="search">
-           <div class= "select_area">
-             <i class="fas fa-map-marker-alt map_icon"></i>
-             <div class="text">Search</div>
-           </div>
-           
-           <div class="line"></div>
-           
-           <div class = "text_and-icon">
-             <input type="text" class="search_text" id="searchWidgetTrigger" placeholder="Search by Company, Keyword or the designation ..">
-                  <i class="fas fa-search search_icon"></i>
-           </div>
-      </div>  
+      </form>
      
     </main>
   );
