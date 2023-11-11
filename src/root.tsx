35c50b676dedf558256c66d6f2a4d566b125691e
Swami Chandrasekaran <swamichandra@gmail.com>
2023-11-10 23:39:43 -0600

    Update root.tsx

--- src/root.tsx
+++ src/root.tsx
@@ -21,16 +21,16 @@ export default function Root() {
         <Title>SolidStart - Bare</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
-        <!-- Widget JavaScript bundle -->
+        
         <script src="https://cloud.google.com/ai/gen-app-builder/client?hl=en_US"></script>
         
-        <!-- Search widget element is not visible by default -->
+        
         <gen-search-widget
           configId="23df4038-8851-4b5e-84ef-86eae2f5ecc4"
           triggerId="searchWidgetTrigger">
         </gen-search-widget>
         
-        <!-- Element that opens the widget on click. It does not have to be an input -->
+        
         <input placeholder="Search here" id="searchWidgetTrigger" />
       </Head>
       <Body>
