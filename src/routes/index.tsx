a23f1387336d1ff2e4e6d964b66448ea5162dc78
Swami Chandrasekaran <swamichandra@gmail.com>
2023-11-11 00:46:49 -0600

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,23 +4,16 @@ export default function Home() {
   return (
     <main>
       <Title>Student of the Client</Title>
-      <h3>Student of the Client!</h3>
+      
 
       <gen-search-widget
             configId="23df4038-8851-4b5e-84ef-86eae2f5ecc4"
             triggerId="searchWidgetTrigger">
       </gen-search-widget>
-      
-      <form>
-       
-            <button type="button">
-                <i class="fa fa-search" aria-hidden="true"></i>
-            </button>
-      </form>
 
       
       <div class="searchBox">
-            <input class="searchInput" type="text" placeholder="Search here" id="searchWidgetTrigger" />
+            <input class="searchInput" type="text" placeholder="Discover insights about your clients here" id="searchWidgetTrigger" />
             <button class="searchButton" href="#">
                 <i class="material-icons">
                     search
