4a64830ffeb79274b517837b805a8fab823a5345
Swami Chandrasekaran <swamichandra@gmail.com>
2023-11-11 12:43:57 -0600

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,28 +1,22 @@
 import { Title } from "solid-start";
 
 export default function Home() {
-  return (
+  return (    
     <main>
-      <Title>Student of the Client</Title>
+    	<h1>Studenf of the Client</h1>
       
-
       <gen-search-widget
             configId="23df4038-8851-4b5e-84ef-86eae2f5ecc4"
             triggerId="searchWidgetTrigger">
       </gen-search-widget>
-
-
-      <div class="flexbox">
-        <div class="search">
-          <h1>Studenf of the Client</h1>
-          <h3>Click on search icon, to discover client insights.</h3>
-          <div>
-            <input type="text" class="searchInput" placeholder="Discover client insights" id="searchWidgetTrigger" />
-          </div>
-        </div>
-     </div>
-
-      
+    	
+    	<div class="SearchBox">
+        <input type="text" class="SearchBox-input" placeholder="Discover client insights" id="searchWidgetTrigger" />
+    	
+    			<button class="SearchBox-button">
+    				<i class="SearchBox-icon  material-icons">discover</i>
+    			</button>
+    	</div>
     </main>
   );
 }
