9a25ba9cd9315abc048ef883ed383ba355149b5d
Swami Chandrasekaran <swamichandra@gmail.com>
2023-11-13 16:48:31 -0600

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,16 +3,13 @@ import { Title } from "solid-start";
 export default function Home() {
   return (
     <main>
-      <Title>Discover Client Insights</Title>
+      <Title>Discover Client Insights <div id="cursor"></div></Title>
       
       <header>
           <h1 class="header-1">Discover Client Insights</h1>
       </header>
 
       <div class='top'></div>
-<div class='text'>
-  <h2> </h2><div id="cursor"></div>
-</div>
 
       <gen-search-widget
             configId="23df4038-8851-4b5e-84ef-86eae2f5ecc4"
