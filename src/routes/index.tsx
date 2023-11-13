45b0e08449efe1636a7a57a9db316368b7c0e004
Swami Chandrasekaran <swamichandra@gmail.com>
2023-11-13 16:55:11 -0600

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <Title>Discover Client Insights</Title>
       
       <header>
-          <h1 class="header-1">Discover Client Insights </h1><div id="cursor"></div>
+          <h1 class="header-1">Discover Client Insights</div></h1><div id="cursor">
       </header>
 
       <div class='top'></div>
