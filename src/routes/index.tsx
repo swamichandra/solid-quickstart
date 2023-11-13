d4a598502c13407925037c1dbb60609d6404e7db
Swami Chandrasekaran <swamichandra@gmail.com>
2023-11-13 16:43:27 -0600

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,9 +9,10 @@ export default function Home() {
           <h1 class="header-1">KPMG: Discover Client Insights/h1>
       </header>
 
-    <div class='text'>
-      <h2>Ask </h2><div id="cursor"></div>
-    </div>
+      <div class='top'></div>
+      <div class='text'>
+        <h2>Ask </h2><div id="cursor"></div>
+      </div>
 
       <gen-search-widget
             configId="23df4038-8851-4b5e-84ef-86eae2f5ecc4"
