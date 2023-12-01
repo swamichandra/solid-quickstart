0094d1651954073380cb844e7afc748f43f01c7e
Swami Chandrasekaran <swamichandra@gmail.com>
2023-12-01 13:11:40 -0600

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,12 +4,6 @@ export default function Home() {
   return (
     <main>
       <Title>Discover Client Insights</Title>
-      
-      <header>
-          <h1 class="header-1">Discover Client Insights</h1><div id="cursor"></div>
-      </header>
-
-      <div class='top'></div>
 
       <gen-search-widget
             configId="23df4038-8851-4b5e-84ef-86eae2f5ecc4"
@@ -18,10 +12,10 @@ export default function Home() {
 
     <div class="flexbox">
       <div class="search">
-        <h1>Search this site</h1>
-        <h3>Click on search icon, then type your keyword.</h3>
+        <h1>Discover Client Insights</h1>
+        <h3>Click on search icon, then type what you would want to know about a client.</h3>
         <div>
-          <input type="text" id="searchWidgetTrigger" placeholder="Discover client insights . . ." required>
+          <input type="text" id="searchWidgetTrigger" placeholder="Discover . . ."  />
         </div>
       </div>
     </div>
