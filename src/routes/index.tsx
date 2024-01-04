275c854416c1a5b54aaeab6de5b4ff07d4ae4795
Swami Chandrasekaran <swamichandra@gmail.com>
2024-01-04 14:54:06 -0600

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <Title>Discover Client Insights</Title>
 
       <gen-search-widget
-            configId="f93d75f5-4d53-439c-9194-f075086d3eb9"
+            configId="23df4038-8851-4b5e-84ef-86eae2f5ecc4"
             location="us"
             triggerId="searchWidgetTrigger">
       </gen-search-widget>
